98793fea4eadb81ea7baf3d81a19600ea8ac1e57
cpalencia886 <c.palencia886@mybvc.ca>
2024-03-09 17:35:07 -0700

    first commit

---
+++ routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Home page');
+});
+
+router.get('/login', (req, res) => {
+  res.send('Login page');
+});
+
+router.get('/register', (req, res) => {
+  res.send('Register page');
+});
+
+router.get('*', (req, res) => {
+  res.send('Page not found');
+});
+
+module.exports = router;
